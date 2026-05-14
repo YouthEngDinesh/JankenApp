b60792cfde37699249fc097ca1eb28b8384f0743
Gupta Dinesh <dinesh.k.gupta@youtheng.co.jp>
2026-05-14 11:18:21 +0900

    name changed

--- Task4_JankenApp/Pages/Index.cshtml.cs
+++ Task4_JankenApp/Pages/Index.cshtml.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Task4_JankenApp.Pages
 {
     public enum Hand
     {
-        Rock,
-        Scissors,
-        Paper
+        Rock,        //0
+        Scissors,    //1
+        Paper        //2
     }
     public class IndexModel : PageModel
     {// 1. Automatically maps the selected HTML input value to this enum property
@@ -15,7 +16,9 @@ namespace Task4_JankenApp.Pages
         public Hand UserHand { get; set; }
 
         // Properties to store game state and pass it back to the HTML View
-        public Hand? CpuHand { get; set; }
+        public Hand? CpuHand { get; set; }           //(?): Making the Enum Nullable to handle the initial state before the game starts
+                                                     //When the application first executes OnGet(), CpuHand is initialized as completely empty(null) [Razor Pages].
+                                                     //the question mark transforms it into a Nullable Enum (Hand?):
         public string GameResult { get; set; }
         public string AlertClass { get; set; }
 
@@ -28,8 +31,11 @@ namespace Task4_JankenApp.Pages
         public void OnPost()
         {
             // Generate a secure random choice for the CPU (0, 1, or 2)
-            var random = new Random();
-            CpuHand = (Hand)random.Next(0, 3);
+            var random = new Random();               // Generates 0, 1, or 2    ...returns a standard integer data type (e.g., 2)      
+            CpuHand = (Hand)random.Next(0, 3);       // Explicit Casting / Enumeration mapping
+                                                     //If the number is 0 --->  it becomes Hand.Rock
+                                                     //If the number is 1 --->  it becomes Hand.Scissors
+                                                     //If the number is 2 --->  it becomes Hand.Paper
 
             // Run the Janken evaluation logic
             EvaluateGame();
@@ -39,19 +45,19 @@ namespace Task4_JankenApp.Pages
         {
             if (UserHand == CpuHand)
             {
-                GameResult = "👔 It's a Tie! (Draw)";
+                GameResult = "👔 It's a Tie! (Draw)_引き分け（あいこ）です。";
                 AlertClass = "warning"; // Yellow alert box
             }
             else if ((UserHand == Hand.Rock && CpuHand == Hand.Scissors) ||
                      (UserHand == Hand.Scissors && CpuHand == Hand.Paper) ||
                      (UserHand == Hand.Paper && CpuHand == Hand.Rock))
             {
-                GameResult = "🎉 Congratulations! You Win!";
+                GameResult = "🎉🎉🎉 Congratulations! You Win!_あなたの【勝ち】です！おめでとう！";
                 AlertClass = "success"; // Green alert box
             }
             else
             {
-                GameResult = "😢 Outplayed! You Lose.";
+                GameResult = "😢 Outplayed! You Lose._あなたの【負け】です。残念…。";
                 AlertClass = "danger"; // Red alert box
             }
         }
